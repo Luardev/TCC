d03528f535dd9ee1aa6cf6039f8f28add9d977de
Luardev <145706514+Luardev@users.noreply.github.com>
2025-02-04 15:46:05 -0300

    Update php.php
    Essa classe de conexao foi testada e aprovada

--- php.php
+++ php.php
@@ -1,3 +1,10 @@
 <?php
- print('oi');
+$servidor = "sql210.infinityfree.com";
+$usuario = "if0_38234357";
+$senha = "senhadobanco1";
+$banco = "if0_38234357_tcc3infob";
+$con = new mysqli($servidor,$usuario,$senha,$banco);
+if ($con -> connect_error){
+    die("Falha de conexão". $con -> connect_error);
+}
 ?>
